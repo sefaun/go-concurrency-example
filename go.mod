33e6624c8bb4ed6845cbd6c3fc4c81e130fece65
Sefa ÜN <sefa.un@tsoft.com.tr>
2022-12-08 10:25:51 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module go-concurrency-example
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+)
+
+type ErrorResponse struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
+func main() {
+
+	request_response := make(chan string)
+
+	go createRequest(request_response)
+	// go createRequest2()
+
+	fmt.Println(<-request_response)
+	fmt.Println("İşlem Bitti")
+}
+
+func createRequest(channel chan string) {
+	fmt.Println("İşlem İçerde")
+	resp, err := http.Get("https://api.uncomsys.com/api/v1/login")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	response_content := ErrorResponse{}
+
+	json.Unmarshal(body, &response_content)
+	fmt.Printf("Message: %s\n", response_content.Message)
+	fmt.Printf("Operation: %s\n", strconv.FormatBool(response_content.Success))
+
+	channel <- string(body)
+}
+
+func createRequest2() {
+	fmt.Println("İşlem İçerde")
+	resp, err := http.Get("https://api.uncomsys.com/api/v1/login")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+
+	response_content := ErrorResponse{}
+
+	json.Unmarshal(body, &response_content)
+	fmt.Printf("Message: %s\n", response_content.Message)
+	fmt.Printf("Operation: %s\n", strconv.FormatBool(response_content.Success))
+}
